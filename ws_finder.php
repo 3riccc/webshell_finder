35f61e53d2e9c8564dee2aa13c611b62614aaafa
Ericccc <ericinisa@163.com>
2015-03-17 19:51:09 +0800

    update to a new version

--- ws_finder.php
+++ ws_finder.php
@@ -1,14 +1,24 @@
 <html>
 <head>
 	<title>webshell检测工具</title>
-	<meta charset="utf8"/>
-</head>
-<p>注意啦：首先请将本文件放在web根目录</p>
+	<meta charset="utf-8"/>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
-<form method="post" action="">
-<input type="hidden" name="find" id="find" value="1"/>
-<input type="submit" value="submit"/>
-</form>
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<div class="container">
+<h1>注意啦：首先请将本文件放在web根目录</h1>
+<a align="center" class="btn btn-lg btn-success" href="?find=1&level=3" role="button"><h4 align="center">以中级安全等级开始检验(推荐)</h4></a><hr/>
+
+<a align="center" class="btn btn-lg btn-success" href="?find=1&level=1" role="button"><h4 align="center">以顶级安全开始检验(误报率高、不推荐)</h4></a><hr/>
+
+<a align="center" class="btn btn-lg btn-success" href="?find=1&level=5" role="button"><h4 align="center">以低级安全等级开始检验(可能放过木马、不推荐)</h4></a><hr/>
+</div>
 
 <?php
 /*
@@ -17,24 +27,21 @@
 *At HDUISA
 */
 
-@$find = $_POST["find"];
-#这个数组是大马的敏感函数，更新的时候可以直接更新数组内容，注意敏感函数只写名字，不写括号
-#$arr = array("1111","222");
-#小马的敏感函数，注意同上
-#$arr_ass = array("1111","2222");
-function listDir($dir){
+@$find = $_REQUEST["find"];
+@$level = $_REQUEST["level"];
+function listDir($dir,$level){
 	if(is_dir($dir)){
 		if($dh = opendir($dir)){
 			while(($file = readdir($dh))){
 				if(is_dir($dir."/".$file) && $file != "." && $file != ".."){
-					echo $dir."/".$file;
-					listDir($dir."/".$file);	
+					listDir($dir."/".$file,$level);	
 				}
-				elseif((string)$file[strlen($file)-1]=="p"&&(string)$file[strlen($file)-2]=="h" && $file!="WebSell_finder.php"){
+				elseif((string)$file[strlen($file)-1]=="p"&&(string)$file[strlen($file)-2]=="h" && $file!="ws_finder.php"){
 
-					$result = checkFile($dir."/".$file);
+					$result = checkFile($dir."/".$file,$level);
 					if($result != "safe"){
-						echo "<br/>".$dir."/".$file."可能是".$result."<br/>";
+						echo "<h2>发现威胁</h2>";
+						echo "<br/>"."<code>".$dir."/".$file."可能是".$result."<br/></code><hr/>";
 					}
 
 				}else{
@@ -46,7 +53,7 @@ function listDir($dir){
 	}
 }
 #这个函数检查特定文件是否是webshell
-function checkFile($thisFile){
+function checkFile($thisFile,$level){
 	$file = fopen($thisFile,"r");
 	#下一个变量是危险指数，危险指数是一个文件中检测出的敏感函数的个数，当一个文件的危险指数大于3时，被认为可能是大马
 	$dangIndex = 0;
@@ -60,10 +67,9 @@ function checkFile($thisFile){
 		if(checkAssShell($string)==1)
 			$assIndex++;
 		}
-	//	var_dump($dangIndex);
 	}
 	#返回这个文件的检测结果
-	if($dangIndex>3){
+	if($dangIndex>$level){
 		return "webShell";
 	}elseif($assIndex>=1){
 		return "smallSehll";
@@ -73,27 +79,40 @@ function checkFile($thisFile){
 	
 	fclose($file);
 }
+
+
+
 #这个函数检测一个字符串中是否包含敏感php函数
 function checkShell($string){
-	foreach(array("command","system","fsockopen","disk_free_space","disk_total_space","php_uname") as &$sth){
+	#这个数组是大马的敏感函数，更新的时候可以直接更新数组内容，注意敏感函数只写名字，不写括号
+	$arr = array("php_sapi_name","get_current_user","readdir","filesize","filetype","command","system","fsockopen","disk_free_space","disk_total_space","php_uname");
+
+	foreach($arr as &$sth){
 		if(strstr($string,$sth)!=false){
 			return 1;
 		}
 	}
 	return 0;
 }
+
+
+
 #这个函数检测一个字符串中是否包含小马的敏感函数
 function checkAssShell($string){
-	foreach(array("eval") as $sth){
+	#小马的敏感函数，注意同上
+	$arr_ass = array("eval");
+
+	foreach($arr_ass as $sth){
 		if(strstr($string,$sth)!=false){
-//			echo (int)strstr($string,$sth);
-	//		echo $string."---------".$sth;
-	//		echo strpos($string,$sth);
                 	return 1;
         	}
         }
         return 0;
 }
+
+#如果用户传递了参数过来，则开始递归调用
 if($find==1){
-	listDir(getcwd());
+	echo '<div class="container">';
+	listDir(getcwd(),$level);
+	echo '</div>';
 }
